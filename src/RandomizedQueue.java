53dbc2b6b135da436c1e3b70fef81111cac77f0a
Paul Moore <pdmoore@gmail.com>
2017-11-11 14:35:46 -0800

    initial impl of RandomizedQueue

---
+++ src/RandomizedQueue.java
@@ -0,0 +1,9 @@
+public class RandomizedQueue<Item> {
+    public boolean isEmpty() {
+        return true;
+    }
+
+    public int size() {
+        return 0;
+    }
+}
---
+++ test/RandomizedQueueTest.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RandomizedQueueTest {
+
+/*
+    public class RandomizedQueue<Item> implements Iterable<Item> {
+        public RandomizedQueue()                 // construct an empty randomized queue
+        public boolean isEmpty()                 // is the randomized queue empty?
+        public int size()                        // return the number of items on the randomized queue
+        public void enqueue(Item item)           // add the item
+        public Item dequeue()                    // remove and return a random item
+        public Item sample()                     // return a random item (but do not remove it)
+        public Iterator<Item> iterator()         // return an independent iterator over items in random order
+        public static void main(String[] args)   // unit testing (optional)
+ */
+
+//
+//    Throw a java.lang.IllegalArgumentException if the client calls enqueue() with a null argument.
+//    Throw a java.util.NoSuchElementException if the client calls either sample() or dequeue() when the randomized queue is empty.
+//
+//    Throw a java.util.NoSuchElementException if the client calls the next() method in the iterator when there are no more items to return.
+//    Throw a java.lang.UnsupportedOperationException if the client calls the remove() method in the iterator.
+
+
+    @Test
+    public void RandomizedQueueStartsOffEmpty() {
+        RandomizedQueue<String> rq = new RandomizedQueue<>();
+        assertTrue(rq.isEmpty(), "newly constructed queue starts off empty");
+    }
+
+    @Test
+    public void RandomizedQueueStartsWithSize_0() {
+        RandomizedQueue<String> rq = new RandomizedQueue<>();
+        assertEquals(0, rq.size(), "newly constructed queue starts off with size zero");
+    }
+}
