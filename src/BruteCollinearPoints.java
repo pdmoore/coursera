8b9ce7f33b97c8422742fe516b7412bef2275f0c
Paul Moore <pdmoore@gmail.com>
2017-11-19 19:51:17 -0800

    remove errant import of java.awt.*

--- src/BruteCollinearPoints.java
+++ src/BruteCollinearPoints.java
@@ -2,7 +2,6 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
 
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
