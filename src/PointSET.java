17c8b8757eaf4c35d563fecf7eb7677e9f5fe165
Paul Moore <pdmoore@gmail.com>
2017-12-03 09:31:12 -0800

    refactor out tracking number of points, use TreeSet methods instead

--- src/PointSET.java
+++ src/PointSET.java
@@ -5,37 +5,33 @@ import java.util.TreeSet;
 
 public class PointSET {
 
-
-    private int pointCount;
     private Set<Point2D> pointSet;
 
     public PointSET() {
-        pointCount = 0;
         pointSet = new TreeSet();
     }
 
     public boolean isEmpty() {
-        return pointCount == 0;
+        return pointSet.isEmpty();
     }
 
     public void insert(Point2D p) {
         if (p == null) throw new IllegalArgumentException();
 
-        pointCount++;
         pointSet.add(p);
     }
 
     public int size() {
-        return pointCount;
-    }
-
-    public static void main(String[] args) {
-
+        return pointSet.size();
     }
 
     public boolean contains(Point2D p) {
         if (p == null) throw new IllegalArgumentException();
 
         return pointSet.contains(p);
+    }
+
+    public static void main(String[] args) {
+
     }
 }
