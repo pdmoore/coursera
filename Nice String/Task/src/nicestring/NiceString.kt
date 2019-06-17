9176edb4b05e2a0a2962849570a2e47518babc8d
Paul Moore <pdmoore@gmail.com>
2019-06-17 10:38:40 -0700

    refactor to shorter check for b*

--- Nice String/Task/src/nicestring/NiceString.kt
+++ Nice String/Task/src/nicestring/NiceString.kt
@@ -2,18 +2,19 @@ package nicestring
 
 fun String.isNice(): Boolean {
 
+    val noBadSubstring = !contains("ba") && !contains("be") && !contains("bu")
+
     val hasThreeVowels = count { it in "aeiou" } >= 3
 
     val pairs = this.toList().zipWithNext();
 
     val hasPair = checkForPair(pairs)
-    val noBvowels = checkForBvowel(pairs)
 
-    if (!noBvowels) {
+    if (!noBadSubstring) {
         return (hasThreeVowels) && (hasPair);
     }
 
-    if (noBvowels) {
+    if (noBadSubstring) {
         if ((hasThreeVowels) || hasPair) return true
     }
 
@@ -21,17 +22,8 @@ fun String.isNice(): Boolean {
     return false;
 }
 
-fun checkForBvowel(pairs: List<Pair<Char, Char>>): Boolean {
-    pairs.forEach { if ((it.first == 'b') &&
-            (it.second == 'u' || it.second == 'a' || it.second == 'e')) return false
-    }
-
-    return true
-}
-
 fun checkForPair(pairs: List<Pair<Char, Char>>): Boolean {
     pairs.forEach { if (it.first == it.second) return true }
-
 
     return false
 }
