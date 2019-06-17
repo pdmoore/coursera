1a5ca41c18207271e5f909ec6d19b91383a598c0
Paul Moore <pdmoore@gmail.com>
2019-06-17 15:53:34 -0700

    java style impl for task 2; short circuit for minTrips==0 case

--- Taxi Park/Task/src/taxipark/TaxiParkTask.kt
+++ Taxi Park/Task/src/taxipark/TaxiParkTask.kt
@@ -10,8 +10,34 @@ fun TaxiPark.findFakeDrivers(): Set<Driver> =
 /*
  * Task #2. Find all the clients who completed at least the given number of trips.
  */
-fun TaxiPark.findFaithfulPassengers(minTrips: Int): Set<Passenger> =
-        TODO()
+fun TaxiPark.findFaithfulPassengers(minTrips: Int): Set<Passenger> {
+    if (minTrips == 0) return allPassengers
+
+    // allPassengers
+    // each trip can have 1 or more passengers
+    // count each time a passenger is seen in a trip
+    // filter by those counts against minTrips
+    val passengerTripCounts:HashMap<Passenger, Int?> = HashMap()
+    for (trip in trips) {
+        for (passenger in trip.passengers) {
+            if (passengerTripCounts.containsKey(passenger)) {
+                var currentCount = passengerTripCounts.get(passenger)
+                passengerTripCounts.replace(passenger, currentCount?.plus(1))
+            } else {
+                passengerTripCounts.put(passenger, 1)
+            }
+        }
+    }
+
+    val qualifyingPassengers:HashSet<Passenger> = HashSet()
+    for (key in passengerTripCounts.keys) {
+        val tripCount = passengerTripCounts?.get(key) ?: 0
+        if (tripCount >= minTrips) qualifyingPassengers.add(key)
+    }
+
+
+    return qualifyingPassengers.toSet()
+}
 
 /*
  * Task #3. Find all the passengers, who were taken by a given driver more than once.
