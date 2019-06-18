c704ff9d24ba5686469a714705db99dd46d7edc4
Paul Moore <pdmoore@gmail.com>
2019-06-17 17:06:17 -0700

    refactor to not need guard clause - ID passengers who don't meet criteria and remove them from allPassengers result

--- Taxi Park/Task/src/taxipark/TaxiParkTask.kt
+++ Taxi Park/Task/src/taxipark/TaxiParkTask.kt
@@ -11,20 +11,19 @@ fun TaxiPark.findFakeDrivers(): Set<Driver> =
  * Task #2. Find all the clients who completed at least the given number of trips.
  */
 fun TaxiPark.findFaithfulPassengers(minTrips: Int): Set<Passenger> {
-    if (minTrips == 0) return allPassengers
-
     // allPassengers
     // each trip can have 1 or more passengers
     // count each time a passenger is seen in a trip
     // filter by those counts against minTrips
     val passengerTripCounts:HashMap<Passenger, Int?> = HashMap()
+    for (passenger in allPassengers) {
+        passengerTripCounts.put(passenger, 0)
+    }
     for (trip in trips) {
         for (passenger in trip.passengers) {
             if (passengerTripCounts.containsKey(passenger)) {
                 var currentCount = passengerTripCounts.get(passenger)
                 passengerTripCounts.replace(passenger, currentCount?.plus(1))
-            } else {
-                passengerTripCounts.put(passenger, 1)
             }
         }
     }
@@ -32,11 +31,10 @@ fun TaxiPark.findFaithfulPassengers(minTrips: Int): Set<Passenger> {
     val qualifyingPassengers:HashSet<Passenger> = HashSet()
     for (key in passengerTripCounts.keys) {
         val tripCount = passengerTripCounts?.get(key) ?: 0
-        if (tripCount >= minTrips) qualifyingPassengers.add(key)
+        if (tripCount < minTrips) qualifyingPassengers.add(key)
     }
 
-
-    return qualifyingPassengers.toSet()
+    return allPassengers.minus(qualifyingPassengers)
 }
 
 /*
