127199b48761bd75a2e1973d9278bbe8707c49e3
Paul Moore <pdmoore@gmail.com>
2019-07-03 13:35:37 -0700

    Week 4 board - add init{} for creating cells

--- 2019-kotlin/Kotlin for Java Developers. Week 4/Board/Task/src/board/BoardImpl.kt
+++ 2019-kotlin/Kotlin for Java Developers. Week 4/Board/Task/src/board/BoardImpl.kt
@@ -14,20 +14,22 @@ fun createSquareBoard(width: Int): SquareBoard {
 fun <T> createGameBoard(width: Int): GameBoard<T> = TODO()
 
 data class BoardImpl(override val width: Int) : SquareBoard {
-    override fun getCell(i: Int, j: Int): Cell {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    val listCells = mutableListOf<Cell>()
 
-    override fun getAllCells(): Collection<Cell> {
-        val listCells = mutableListOf<Cell>()
-
+    init {
         for (i in 1..width) {
             for (j in 1..width) {
                 val cell = Cell(i, j)
                 listCells.add(cell)
             }
         }
+    }
 
+    override fun getCell(i: Int, j: Int): Cell {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getAllCells(): Collection<Cell> {
         return listCells
     }
 
