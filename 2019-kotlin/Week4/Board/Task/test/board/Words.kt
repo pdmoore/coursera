e4be598409b74a7ebd8253f87ea25e8a88d53287
Paul Moore <pdmoore@gmail.com>
2019-07-26 13:32:33 -0700

    week 5 playground exercise - Words

---
+++ 2019-kotlin/Week4/Board/Task/test/board/Words.kt
@@ -0,0 +1,32 @@
+package board
+
+import junit.framework.Assert.assertEquals
+
+// Week 5 - Playground Member extensions
+
+class Words {
+    private val list = mutableListOf<String>()
+
+    fun String.record() {
+        list.add(this)
+    }
+
+    operator fun String.unaryPlus() {
+        record()
+    }
+
+    override fun toString() = list.toString()
+}
+
+
+fun main(args: Array<String>) {
+    val words = Words()
+    with(words) {
+        "one".record()
+        +"two"
+    }
+    assertEquals("[one, two]", words.toString())
+}
+
+
+
